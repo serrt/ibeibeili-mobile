17c2635d26e2f30709fef11a92d911ec6c3cc3c9
lym125 <lym125@gmail.com>
2016-11-23 16:47:31 +0800

    Api 请求根地址

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import routes from './routes'
+import appEnv from '../env'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
+
+// Api 请求根地址
+Vue.http.options.root = appEnv.apiUrl
 
 const router = new VueRouter({
   mode: 'history',
