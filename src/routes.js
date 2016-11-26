b05b2ef18b20e7e1255ae09e65de56829e8ed1f6
lym125 <lym125@gmail.com>
2016-11-26 15:54:00 +0800

    路由懒加载

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,14 @@
 const routes = [{
   path: '/',
   name: 'home',
-  component: (resolve) => {
-    require.ensure(['./views/Home.vue'], () => {
-      resolve(require('./views/Home.vue'))
-    })
+  component (resolve) {
+    require(['./views/Home.vue'], (resolve))
   }
 }, {
   path: '*',
   name: '404',
-  component: (resolve) => {
-    require.ensure(['./views/NotFound.vue'], () => {
-      resolve(require('./views/NotFound.vue'))
-    })
+  component (resolve) {
+    require(['./views/NotFound.vue'], (resolve))
   }
 }]
 
