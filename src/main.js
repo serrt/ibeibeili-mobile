951fec7de3be9c65a7413212b57b38b93b03b3be
lym125 <lym125@gmail.com>
2016-12-05 10:23:15 +0800

    axios 异步加载服务端数据

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
 import routes from './routes'
+import appEnv from '../env'
 
 Vue.use(VueRouter)
 
+axios.defaults.baseURL = appEnv.apiUrl
+axios.interceptors.request.use((config) => {
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+axios.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  if (error.response) {
+    if (error.response.status === 404) {
+      console.log('请求没有找到')
+    } else if (error.response.status === 405) {
+      console.log('方法不被允许')
+    }
+  } else {
+    console.log('Error', error.message)
+  }
+  return Promise.reject(error)
+})
+
+Vue.prototype.$http = axios
+
 const router = new VueRouter({
-  mode: 'history',
   routes
 })
 
