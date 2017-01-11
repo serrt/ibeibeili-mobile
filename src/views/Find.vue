8e43229504683d10b3aadec99e9f2ad3876f0593
panliang <1163816051@qq.com>
2017-01-11 16:38:19 +0800

    Find.vue

--- src/views/Find.vue
+++ src/views/Find.vue
@@ -57,20 +57,18 @@
           </li>
         </ul>
       </div>
-      <div class="page-loadmore-wrapper latest-news" ref="wrapper" :style="{height: wrapperHeight + 'px' }">
-        <loadmore :autoFill="false" :top-method="refresh" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore">
-          <ul>
-            <li v-for="item in list">
-              <div class="words fl">
-                <span class="abstract">{{item.title}}</span>
-              </div>
-              <span class="date">{{item.published_at}}</span>
-              <div class="news-pic flex-middle">
-                <img v-bind:src="item.image">
-              </div>
-            </li>
-          </ul>
-        </loadmore>
+      <div class="latest-news">
+        <ul>
+          <li v-for="item in list">
+            <div class="words fl">
+              <span class="abstract">{{item.title}}</span>
+            </div>
+            <span class="date">{{item.published_at}}</span>
+            <div class="news-pic flex-middle">
+              <img v-bind:src="item.image">
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
     <footer-nav></footer-nav>
@@ -86,57 +84,21 @@ export default {
   data: function () {
     return {
       title: '发现',
-      api: [
+      list: [
         {id: 1, title: '1倍倍利最红11月讲真，大红包限时抢，精彩不容错过！倍倍利最红11月讲真，大红包限时抢，精彩不容错过！', published_at: '2016-12-24 12:24:58', image: './static/images/news.png'},
         {id: 2, title: '2倍倍利最红11月讲真，大红包限时抢，精彩不容错过！倍倍利最红11月讲真，大红包限时抢，精彩不容错过！', published_at: '2016-11-24 12:24:58', image: './static/images/news.png'},
         {id: 3, title: '3倍倍利最红11月讲真，大红包限时抢，精彩不容错过！倍倍利最红11月讲真，大红包限时抢，精彩不容错过！', published_at: '2016-10-24 12:24:58', image: './static/images/news.png'},
         {id: 3, title: '4倍倍利最红11月讲真，大红包限时抢，精彩不容错过！倍倍利最红11月讲真，大红包限时抢，精彩不容错过！', published_at: '2016-10-24 12:24:58', image: './static/images/news.png'},
         {id: 3, title: '5倍倍利最红11月讲真，大红包限时抢，精彩不容错过！倍倍利最红11月讲真，大红包限时抢，精彩不容错过！', published_at: '2016-10-24 12:24:58', image: './static/images/news.png'},
         {id: 4, title: '6倍倍利最红11月讲真，大红包限时抢，精彩不容错过！倍倍利最红11月讲真，大红包限时抢，精彩不容错过！', published_at: '2016-09-24 12:24:58', image: './static/images/news.png'}
-      ],
-      list: [],
-      allLoaded: true,
-      wrapperHeight: 0,
-      bottomStatus: '',
-      topStatus: ''
+      ]
     }
   },
   mounted () {
-    this.wrapperHeight = document.documentElement.clientHeight - this.$refs.wrapper.getBoundingClientRect().top
-    this.refresh(1)
   },
   computed: {
   },
   methods: {
-    handleListChange: function (value) {
-      this.list = value
-    },
-    loadData: function (id, dir) {
-      let dataList = this.api
-      this.list = this.list.concat(dataList)
-      if (dir === 'top') {
-        this.$refs.loadmore.onBottomLoaded(id)
-      } else if (dir === 'bottom') {
-        this.$refs.loadmore.onTopLoaded(id)
-      }
-      // this.$http.get(this.api).then((response) => {
-      //   let dataList = response.data.data
-      //   this.list = this.list.concat(dataList)
-      //   if (dir === 'top') {
-      //     this.$refs.loadmore.onBottomLoaded(id)
-      //   } else if (dir === 'bottom') {
-      //     this.$refs.loadmore.onTopLoaded(id)
-      //   }
-      // })
-      // this.allLoaded = true
-    },
-    loadBottom: function (id) {
-      this.loadData(id, 'top')
-    },
-    refresh: function (id) {
-      this.list = []
-      this.loadData(id, 'bottom')
-    }
   },
   watch: {
   }
